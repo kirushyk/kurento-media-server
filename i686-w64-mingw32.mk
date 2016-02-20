5148a1d76c93ab52e94aa35cab0354e25c0e2b7a
Kyrylo Polezhaiev <polezhaiev@ukr.net>
2016-02-20 16:38:01 +0200

    mingw32: Link against libboost_log-mt libboost_program_options-mt

--- i686-w64-mingw32.mk
+++ i686-w64-mingw32.mk
@@ -6,7 +6,7 @@ CXXFLAGS=--std=gnu++17
 
 CFLAGS=-DWIN32_LEAN_AND_MEAN=1 -Dushort="unsigned short" -Duint="unsigned" -mms-bitfields -I. -I/usr/i686-w64-mingw32/sys-root/mingw/include/gstreamer-1.0 -I/usr/i686-w64-mingw32/sys-root/mingw/lib/gstreamer-1.0/include -I/usr/i686-w64-mingw32/sys-root/mingw/include/glibmm-2.4 -I/usr/i686-w64-mingw32/sys-root/mingw/lib/glibmm-2.4/include -I/usr/i686-w64-mingw32/sys-root/mingw/include/glib-2.0 -I/usr/i686-w64-mingw32/sys-root/mingw/lib/glib-2.0/include -I/usr/i686-w64-mingw32/sys-root/mingw/include/sigc++-2.0 -I/usr/i686-w64-mingw32/sys-root/mingw/lib/sigc++-2.0/include -I../kms-jsonrpc/src/ -I../kms-jsonrpc/src/jsonrpc/ -I../jsoncpp/include/ -I../kms-core/src/server/interface/ -I../kms-core/src/server/implementation/ -I../kms-core/src/server/implementation/objects/ -I../kurento-media-server/server/transport/ -I../libevent/include/ -I./win32/ -I./server/ -I./server/transport/websocket/
 
-LIBS=-L/usr/i686-w64-mingw32/sys-root/mingw/lib -lgstreamer-1.0 -lglibmm-2.4 -lgobject-2.0 -lglib-2.0 -lintl -lsigc-2.0 -lboost_system-mt
+LIBS=-L/usr/i686-w64-mingw32/sys-root/mingw/lib -lgstreamer-1.0 -lglibmm-2.4 -lgobject-2.0 -lglib-2.0 -lintl -lsigc-2.0 -lboost_system-mt -lboost_log-mt -lboost_program_options-mt
 
 SRC=./server/CacheEntry.cpp \
 ./server/transport/TransportFactory.cpp \
